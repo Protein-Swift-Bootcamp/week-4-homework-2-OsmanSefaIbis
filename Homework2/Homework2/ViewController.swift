efa53ac3428ff4338ab66f105da5a7e243237c38
Sefa IBIS <osmansefaibis@gmail.com>
2022-12-27 02:10:15 +0300

    Initiated project
    Just runnable project, nothing done yet.

---
+++ Homework2/Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Sefa İbiş on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
